7f21503cb1cce35ccc8d375ce2bd927b9eeaa07d
Andres Boni <andresboni91@gmail.com>
2024-07-22 22:37:49 +1200

    feat(couponslist): ✨ add Redux for coupon list modal management

---
+++ frontend/src/store/slices/modalSlice.ts
@@ -0,0 +1,44 @@
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { Coupon } from "@/types";
+
+export interface ModalState {
+  couponModal: { isOpen: boolean; coupon?: Coupon };
+  redeemModal: { isOpen: boolean; coupon?: Coupon };
+}
+
+const initialState: ModalState = {
+  couponModal: { isOpen: false },
+  redeemModal: { isOpen: false },
+};
+
+const modalSlice = createSlice({
+  name: "modals",
+  initialState,
+  reducers: {
+    openCouponModal: (state, action: PayloadAction<Coupon>) => {
+      state.couponModal.isOpen = true;
+      state.couponModal.coupon = action.payload;
+    },
+    closeCouponModal: (state) => {
+      state.couponModal.isOpen = false;
+      state.couponModal.coupon = undefined;
+    },
+    openRedeemModal: (state, action: PayloadAction<Coupon>) => {
+      state.couponModal.isOpen = false;
+      state.redeemModal.isOpen = true;
+      state.redeemModal.coupon = action.payload;
+    },
+    closeRedeemModal: (state) => {
+      state.redeemModal.isOpen = false;
+      state.redeemModal.coupon = undefined;
+    },
+  },
+});
+
+export const {
+  openCouponModal,
+  closeCouponModal,
+  openRedeemModal,
+  closeRedeemModal,
+} = modalSlice.actions;
+export default modalSlice.reducer;
