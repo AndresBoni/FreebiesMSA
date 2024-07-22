531724a1ac8a94a47b16068fbe38561def488d0d
Andres Boni <andresboni91@gmail.com>
2024-07-23 01:49:16 +1200

    feat: ✨ add location filter for coupons with New Zealand states/districts
    - Added a Select component for filtering coupons based on New Zealand states/districts.
    - Implemented URL-based filtering for the coupon list.
    - Included New Zealand location data for filtering.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,15 @@
 import React from "react";
 import Navbar from "./components/Navbar/Navbar";
 import CustomerHomePage from "./pages/CustomerHomePage/CustomerHomePage";
+import { Route, Routes } from "react-router-dom";
 
 const App: React.FC = () => {
   return (
     <>
       <Navbar />
-      <CustomerHomePage />
+      <Routes>
+        <Route path="/:location?/:district?" element={<CustomerHomePage />} />
+      </Routes>
     </>
   );
 };
