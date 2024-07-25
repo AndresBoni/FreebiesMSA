24617ad63278ba0b54d4076a8ba9de368f31ee55
Andres Boni <andresboni91@gmail.com>
2024-07-26 01:40:12 +1200

    feat(auth & footer): ✨ add Login and SignUp pages, and global Footer component with copyright
    - Added Login page for user authentication.
    - Added SignUp page for new user registration.
    - Added global Footer component with a copyright notice for the entire application.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,6 +3,9 @@ import Navbar from "./components/Navbar/Navbar";
 import CustomerHomePage from "./pages/CustomerHomePage/CustomerHomePage";
 import { Route, Routes } from "react-router-dom";
 import CompanyHomePage from "./pages/CompanyHomePage/CompanyHomePage";
+import Login from "./pages/Login";
+import SignUp from "./pages/SignUp";
+import Copyright from "./components/Copyright";
 
 const App: React.FC = () => {
   return (
@@ -12,7 +15,10 @@ const App: React.FC = () => {
         {/*TODO implement customer/company redirect*/}
         <Route path="/company" element={<CompanyHomePage />} />
         <Route path="/:location?/:district?" element={<CustomerHomePage />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<SignUp />} />
       </Routes>
+      <Copyright />
     </>
   );
 };
