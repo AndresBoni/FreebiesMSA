4235499b914a9594b9a36df167a3329c16dea4a6
Andres Boni <andresboni91@gmail.com>
2024-08-04 20:01:24 +1200

    fix(auth): 🐛 include missing user role in auth response

--- backend/Dtos/Account/NewUserDto.cs
+++ backend/Dtos/Account/NewUserDto.cs
@@ -5,5 +5,6 @@ namespace backend.Dtos
         public string Name { get; set; }
         public string Email { get; set; }
         public string Token { get; set; }
+        public string Role { get; set; }
     }
 }
